4e81c7d01faf1005e2b7347f244f38255b819861
nneshhh9 <nneshhh167@gmail.com>
2024-07-23 22:38:16 +0500

    create back project

---
+++ price-list-editor-api/price-list-editor-api/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace price_list_editor_api.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
